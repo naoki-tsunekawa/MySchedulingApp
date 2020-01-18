2b5bcafa82f446296c381f60efec0ca2d0d96bb3
naoki-tsunekawa <b1014138@gmail.com>
2020-01-18 22:15:50 +0900

    新規スケジュール作成画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'schedules#index'
+  get 'schedules', to: 'schedules#index'
+  get 'schedules/new', to: 'schedules#new'
 end
