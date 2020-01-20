0476a212ef1e9b30212a1b750830a2b93cb3be63
naoki-tsunekawa <b1014138@gmail.com>
2020-01-20 23:56:16 +0900

    新規スケジュールのデータ保存の実装

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -5,4 +5,14 @@ class SchedulesController < ApplicationController
   def new
     @schedule = Schedule.new
   end
+
+  def create
+    Schedule.create(schedule_params)
+  end
+
+  private
+
+    def schedule_params
+      params.require(:schedules).permit(:name,:title,:body,:deadline,:compleate)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'schedules', to: 'schedules#index'
   get 'schedules/new', to: 'schedules#new'
+  post 'schedules', to: 'schedules#create'
 end
