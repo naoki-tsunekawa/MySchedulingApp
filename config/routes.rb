e8da22329d8b0f1a20eb81568dd54aed6ae4ff49
naoki-tsunekawa <b1014138@gmail.com>
2020-02-03 21:27:07 +0900

    リンクURLの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'schedules', to: 'schedules#index'
-  get 'schedules/new', to: 'schedules#new'
-  post 'schedules', to: 'schedules#create'
-  get 'schedules/:id', to: 'schedules#show'
+  resources :schedules, only: [:index, :create, :new, :show]
 end
