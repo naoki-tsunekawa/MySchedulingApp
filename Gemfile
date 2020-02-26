846a8ab00401d5aa30b032d00210095f389ab650
naoki-tsunekawa <b1014138@gmail.com>
2020-02-26 20:47:44 +0900

    コメントの保存機能の実装

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
   gem 'rspec-rails', '~> 3.8'
+  gem 'rails-flog', require: 'flog'
 end
 
 group :development do
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,21 @@
 class CommentsController < ApplicationController
   def create
+    comment = Comment.new(comment_params)
+    if comment.save
+      flash[:notice] = "コメントを保存しました。"
+      redirect_to comment.schedule
+    else
+      flash[:comment] = comment_params
+      flash[:error_messages] = comment.error.full_messages
+      redirect_back fallback_location: comment.schedule
+    end
   end
 
   def destroy
   end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:schedule_id, :name, :comment)
+    end
 end
